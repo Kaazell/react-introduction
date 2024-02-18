f463ef0b692aff0c3cdb2ad667acee68f2891785
Atef Lachhab <atef.lachhab1@gmail.com>
2024-02-18 11:43:37 -0500

    Lecon sur le redering conditionnel

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { Greetings } from "./Greetings";
 export function App() {
   return (
     <>
-      <p>this is a component</p>
-      <Greetings firstName={"Rachel"} age={30} />
+      <Greetings firstName={"Rachel"} age={10} isSunny={1} />
     </>
   );
 }
--- src/Greetings.jsx
+++ src/Greetings.jsx
@@ -1,10 +1,21 @@
 export function Greetings(props) {
   console.log(props);
+  if (props.age > 100) {
+    return <p>Tu n'es pas jeune</p>;
+  } else {
+    return (
+      <>
+        <p>Hello {props.firstName}</p>
+        <p>You are {props.age} years old</p>
+        <p>{props.isSunny ? <ItsSunny /> : <ItsRainy />}</p>
+      </>
+    );
+  }
+}
 
-  return (
-    <>
-      <p>Hello {props.firstName}</p>
-      <p>You are {props.age} years old</p>
-    </>
-  );
+function ItsSunny() {
+  return <h1>Il fait beau</h1>;
+}
+function ItsRainy() {
+  return <h1>Il pleut </h1>;
 }
